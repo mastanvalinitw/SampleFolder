ba92cca7140751e5813a79561e1630c6f9e1b50a
mastanvalinitw <mastanvali.nitw@gmail.com>
2024-03-29 07:01:59 -0400

    controlflow

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,53 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+
+let isLoggedin = true;
+let userMessage;
+
+if (isLoggedin){
+    if (userRole==="admin"){
+        userMessage="Welcome Admin!";
+    } else {
+        userMessage="Welcome User!";
+    }
+} else {
+    userMessage="Please login to the access system.";
+}
+
+console.log("User Message", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+switch(userType){
+    case "admin":
+        userCategory="Administrator";
+        break;
+    case "manager":
+        userCategory="Manager";
+        break;
+    case "subscriber":
+        userCategory="Subscriber";
+        break;
+    default:
+        userCategory="Unknown";
+}
+
+console.log("User Category:", userCategory);
+
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
